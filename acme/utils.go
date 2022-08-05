1f813575ef28e0ba868d0d28a7c94803f9f3831d
hm-karius <tobias.karius@hm.edu>
2022-08-05 11:45:07 +0200

    implemented publish MQTT message func in utils class

--- acme/utils.go
+++ acme/utils.go
@@ -1,6 +1,12 @@
 package acme
 
+import (
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
 //takes a marshaled json as input parameter and publishes the json as string on given endpoint
-func PublishMQTTMessage(json []byte, endpoint string) {
+func PublishMQTTMessage(client MQTT.Client, json []byte, endpoint string) {
 	//string(json)
+	token := client.Publish(endpoint, 0, false, string(json))
+	token.Wait()
 }
