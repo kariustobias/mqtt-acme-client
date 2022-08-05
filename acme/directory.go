191b77da181253d37038b2979ed3a10d8d549349
hm-karius <tobias.karius@hm.edu>
2022-08-05 11:45:35 +0200

    implemented directory publish

--- acme/directory.go
+++ acme/directory.go
@@ -1,6 +1,23 @@
 package acme
 
-func GetDirectory() {
+import (
+	"encoding/json"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func GetDirectory(client MQTT.Client) {
+
+	//build json
+	json, _ := json.Marshal(&Directory{
+		Path: "/acme/acme/directory",
+	})
+
+	//if err != nil {
+	//	fmt.Println("could not marshal json: %s\n", err)
+	//	return nil
+	//}
+
 	// publish getDirectory request to servers endpoint. Receive response in handler
-	PublishMQTTMessage(nil, "/acme/server")
+	PublishMQTTMessage(client, json, "/acme/server")
 }
