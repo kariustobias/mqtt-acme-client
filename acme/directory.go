15d9af7967824aa034a6f9d2b477dfd58f24b24a
hm-karius <tobias.karius@hm.edu>
2022-08-27 22:55:56 +0200

    added HandleDirectoryResponse method

--- acme/directory.go
+++ acme/directory.go
@@ -2,14 +2,16 @@ package acme
 
 import (
 	"encoding/json"
+	"fmt"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
-func GetDirectory(client MQTT.Client) {
+func GetDirectory(client MQTT.Client) []byte {
+	fmt.Printf("GetDirectory\n")
 
 	//build json
-	json, _ := json.Marshal(&Directory{
+	json, _ := json.Marshal(&DirectoryReq{
 		Path: "/acme/acme/directory",
 	})
 
@@ -19,5 +21,19 @@ func GetDirectory(client MQTT.Client) {
 	//}
 
 	// publish getDirectory request to servers endpoint. Receive response in handler
-	PublishMQTTMessage(client, json, "/acme/server")
+	return json
+}
+
+//parses the MQTT directory response from the acme server into a struct object
+func HandleDirectoryResponse(msg MQTT.Message) *DirectoryResp {
+	fmt.Printf("HandleDirectoryResponse\n")
+	var dir DirectoryResp
+	err := json.Unmarshal(msg.Payload(), &dir)
+	if err != nil {
+		fmt.Printf("couldn't parse msg into DirectoryResp %s\n", err.Error())
+	}
+
+	fmt.Printf("%+v\n", dir)
+	fmt.Printf(string(msg.Payload()) + "\n")
+	return &dir
 }
