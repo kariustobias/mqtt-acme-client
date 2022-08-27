6de9b4dbd2b777437bf59401096428ae082841ce
hm-karius <tobias.karius@hm.edu>
2022-08-27 22:57:06 +0200

    added GenerateKeyPair method

--- acme/utils.go
+++ acme/utils.go
@@ -1,6 +1,10 @@
 package acme
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"fmt"
+
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
@@ -9,4 +13,12 @@ func PublishMQTTMessage(client MQTT.Client, json []byte, endpoint string) {
 	//string(json)
 	token := client.Publish(endpoint, 0, false, string(json))
 	token.Wait()
+}
+
+func GenerateKeyPair() *rsa.PrivateKey {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		fmt.Printf("keypair generation faild: %s", err)
+	}
+	return privateKey
 }
