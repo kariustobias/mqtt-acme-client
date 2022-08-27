bc93d04b459e1443eae235316853031ba61a1e64
hm-karius <tobias.karius@hm.edu>
2022-08-27 22:57:33 +0200

    updated main

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/kariustobias/mqtt-acme-client/acme"
 )
 
@@ -9,5 +11,16 @@ func main() {
 	// publish GETDirectory json on /acme/server endpoint
 	client, _ := acme.Initialize("tcp://localhost:1883", "/acme/client")
 	// publish GetDirectory request. All further requests will be handled by the handler
-	acme.GetDirectory(client)
+	json := acme.GetDirectory(client)
+	acme.PublishMQTTMessage(client, json, "/acme/server")
+
+	nonceManager := nonces.NewManager(doer, dir.NewNonceURL)
+
+	jws := secure.NewJWS(privateKey, kid, nonceManager)
+
+	for !acme.GetFlag() {
+		time.Sleep(1 * time.Second)
+		//fmt.Println("waiting: ", wcount)
+		//wcount += 1
+	}
 }
